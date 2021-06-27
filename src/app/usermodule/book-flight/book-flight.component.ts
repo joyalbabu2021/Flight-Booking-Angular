1f5dad7e0262576468779e6564ccad3e2456f133
Praveen Kumar <git-commit-address>
2021-06-27 20:10:58 +0530

    Add project files.

---
+++ src/app/usermodule/book-flight/book-flight.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-flight',
+  templateUrl: './book-flight.component.html',
+  styles: [
+  ]
+})
+export class BookFlightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/usermodule/mange-bookings/mange-bookings.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mange-bookings',
+  templateUrl: './mange-bookings.component.html',
+  styles: [
+  ]
+})
+export class MangeBookingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
